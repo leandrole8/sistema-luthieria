c00e59559a931b94c59911534dfa51f7b53abb62
leandrole8 <leandro_le8@hotmail.com>
2021-03-07 10:08:50 -0300

    Projeto criado

---
+++ backend/src/main/java/com/leandroreis/luthieria/LuthieriaApiApplication.java
@@ -0,0 +1,13 @@
+package com.leandroreis.luthieria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuthieriaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuthieriaApiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/leandroreis/luthieria/LuthieriaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leandroreis.luthieria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuthieriaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
